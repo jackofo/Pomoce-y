4808c20509eb1e66ce5d71b6e1c034bcf8c31146
Jacek Jachimczyk <jacek.jachimczyk@gmail.com>
2020-05-21 22:04:02 +0200

    not supposed to be here xD

---
+++ Pomoce/App_Apps/hangman/HangmanCreator.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pomoce.App_Apps.hangman
+{
+    public partial class HangmanCreator : System.Web.UI.Page
+    {
+        List<string> Words { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Words = new List<string>();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text != "" || TextBox1.Text != null)
+            {
+                Words.Add(TextBox1.Text);
+                TextBox1.Text = "";
+                int count = 0;
+
+                if (ViewState["ButtonCount"] != null)
+                {
+                    count = (int)ViewState["ButtonCount"];
+                }
+
+                count++;
+                ViewState["ButtonCount"] = count;
+
+                foreach (var w in Words)
+                {
+                    Literal literal = new Literal();
+                    literal.Text = w;
+                    literal.ID = "lit" + w;
+
+                    UpdatePanel1.ContentTemplateContainer.Controls.Add(literal);
+                }
+            }
+        }
+    }
+}
